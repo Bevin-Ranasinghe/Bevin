2092abf0cb2bd75b12d80faab912bcb7d4d35a8f
Bevin-Ranasinghe <126078683+Bevin-Ranasinghe@users.noreply.github.com>
2025-04-04 10:19:18 +0300

    Delete task4.cpp

--- task4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#define SQUARE(x) ((x) * (x))
-
-int main() {
-    int num;
-    std::cout << "Enter number: ";
-    std::cin >> num; 
-    std::cout << "Square of " << num << " is: " << SQUARE(num) << "\n";
-}
