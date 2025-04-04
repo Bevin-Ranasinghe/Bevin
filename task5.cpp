aa1e00ff28a9779de46b8180a3b39c787ca03020
Bevin-Ranasinghe <126078683+Bevin-Ranasinghe@users.noreply.github.com>
2025-04-04 09:55:59 +0300

    edited

--- task5.cpp
+++ task5.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
 
 int main() {
     int num;
-    
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+
+    int sum = 0;
+
+    // Make a copy of the number and ensure it's positive
+    num = abs(num);
+
+    for (; num > 0; num /= 10) {
+        sum += num % 10;
+    }
+
+    std::cout << "Sum of digits: " << sum << std::endl;
+
+    return 0;
+}
